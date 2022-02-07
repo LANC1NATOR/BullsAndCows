11ac9c0763b2bdd47834078761055dafc16ce322
Mark <butovmark0000@gmail.com>
2022-02-08 00:39:06 +0300

    Final Version

--- bullsandcows.py
+++ bullsandcows.py
@@ -6,23 +6,24 @@ from termcolor import cprint, colored
 while True:
     turn = 0
     generate_number()
-    cprint('Число загадано! Число состоит из 4 неповторяющихся цифр.',
+    cprint('The number is guessed! '
+           'The number consists of 4 non-repeating digits.',
            'yellow')
     while True:
-        user_input = input(colored('Введите Ваше число: ', 'blue'))
+        user_input = input(colored('Enter your number: ', 'blue'))
         cprint(user_input, 'cyan')
         result = check_number(user_input)
         if result is None:
-            cprint('По правилам необходимо ввести четырехзначное число c '
-                   'неповторяющимися цифрами!', 'red')
+            cprint('According to the rules, you must enter a four-digit number'
+                   'with non-repeating digits!', 'red')
         else:
-            cprint(f'> быки - {result["bulls"]}, коровы - {result["cows"]}',
+            cprint(f'> bulls - {result["bulls"]}, cows - {result["cows"]}',
                    'magenta')
             turn += 1
             if result['bulls'] == 4:
                 break
     if result['bulls'] == 4:
-        cprint('Вы выиграли!', 'green')
-        cprint(f'Количество ходов - {turn}', 'green')
-        cprint('Хотите еще партию?', 'green')
+        cprint('You have won!', 'green')
+        cprint(f'Number of moves - {turn}', 'green')
+        cprint('Do you want to play again?', 'green')
         break
